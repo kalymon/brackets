5a580a825a0fe4ff0f1d68f4eca8fc9d0f0d605d
Your Name <kalymonoleg3@gmail.com>
2021-03-15 19:20:25 +0000

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    str = str.split('');
+    for (var i = 0; i < str.length; i++) { //цикл по строке
+        for (var j = 0; j < bracketsConfig.length; j++) { //цикл по конфигу
+            if (bracketsConfig[j][0] == str[i] && bracketsConfig[j][1] == str[i + 1]) {
+                str.splice(i, 2);
+                i--;
+                j = -1;
+            }
+        }
+    }
+    if (str.length == 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
